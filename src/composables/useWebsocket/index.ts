61f9090bc286bf74d255102c3f667ebc9437e18b
ptp_jackhsiung <ptp_jackhsiung@photons.com.tw>
2025-09-25 16:59:51 +0800

    [MF-4151] ws client create step 7

--- src/composables/useWebsocket/index.ts
+++ src/composables/useWebsocket/index.ts
@@ -3,6 +3,7 @@ import { ActionType, type MessageMap } from '@/core/wsClient/types/message'
 import { onMounted, onUnmounted, watchEffect } from 'vue'
 import { useUserStore } from '@/stores/user'
 import { storeToRefs } from 'pinia'
+import { emitter } from '@/core/mitt'
 
 const genObservekey = (data = {}) => {
   return `action_${'action' in data ? data.action : 'other'}`
@@ -28,20 +29,59 @@ window._ws = ws
 export default function useWebsocket() {
   const { wsInfo, state } = storeToRefs(useUserStore())
 
-  const subscribe_drawing = ws.subscribe({ action: ActionType.DRAWING }, (v) => {
-    console.log(`v [${ActionType[ActionType.DRAWING]}] ===> `, v)
-  })
-
-  const subscribe_joinGAME = ws.subscribe({ action: ActionType.JOIN_GAME }, (v: MessageMap[ActionType.JOIN_GAME]) => {
-    console.log(`v [${ActionType[ActionType.JOIN_GAME]}] ===> `, v)
+  const subscribe_joinGAME = ws.subscribe({ action: ActionType.JOIN_GAME }, (m: MessageMap[ActionType.JOIN_GAME]) => {
+    console.log(`v [${ActionType[ActionType.JOIN_GAME]}] ===> `, m)
+    emitter.emit('JOIN_GAME', m)
   })
 
   const subscribe_openBet = ws.subscribe({ action: ActionType.OPEN_BET }, (m: MessageMap[ActionType.OPEN_BET]) => {
     console.log(`v [${ActionType[ActionType.OPEN_BET]}] ===> `, m)
+    emitter.emit('OPEN_BET', m)
+  })
+
+  const subscribe_closeBet = ws.subscribe({ action: ActionType.CLOSE_BET }, (m: MessageMap[ActionType.CLOSE_BET]) => {
+    console.log(`v [${ActionType[ActionType.CLOSE_BET]}] ===> `, m)
+    emitter.emit('CLOSE_BET', m)
+  })
+
+  const subscribe_gameResult = ws.subscribe({ action: ActionType.GAME_RESULT }, (m: MessageMap[ActionType.GAME_RESULT]) => {
+    console.log(`v [${ActionType[ActionType.GAME_RESULT]}] ===> `, m)
+    emitter.emit('GAME_RESULT', m)
+  })
+
+  const subscribe_betResult = ws.subscribe({ action: ActionType.BET_RESULT }, (m: MessageMap[ActionType.BET_RESULT]) => {
+    console.log(`v [${ActionType[ActionType.BET_RESULT]}] ===> `, m)
+    emitter.emit('BET_RESULT', m)
   })
 
   const subscribe_syncTimer = ws.subscribe({ action: ActionType.SYNC_TIMER }, (m: MessageMap[ActionType.SYNC_TIMER]) => {
     console.log(`v [${ActionType[ActionType.SYNC_TIMER]}] ===> `, m)
+    emitter.emit('SYNC_TIMER', m)
+  })
+
+  const subscribe_getHistory = ws.subscribe({ action: ActionType.GET_HISTORY }, (m: MessageMap[ActionType.GET_HISTORY]) => {
+    console.log(`v [${ActionType[ActionType.GET_HISTORY]}] ===> `, m)
+    emitter.emit('GET_HISTORY', m)
+  })
+
+  const subscribe_updateOtherChips = ws.subscribe({ action: ActionType.UPDATE_OTHER_CHIPS }, (m: MessageMap[ActionType.UPDATE_OTHER_CHIPS]) => {
+    console.log(`v [${ActionType[ActionType.UPDATE_OTHER_CHIPS]}] ===> `, m)
+    emitter.emit('UPDATE_OTHER_CHIPS', m)
+  })
+
+  const subscribe_getBallance = ws.subscribe({ action: ActionType.GET_BALANCE }, (m: MessageMap[ActionType.GET_BALANCE]) => {
+    console.log(`v [${ActionType[ActionType.GET_BALANCE]}] ===> `, m)
+    emitter.emit('GET_BALANCE', m)
+  })
+
+  const subscribe_drawing = ws.subscribe({ action: ActionType.DRAWING }, (m: MessageMap[ActionType.DRAWING]) => {
+    console.log(`v [${ActionType[ActionType.DRAWING]}] ===> `, m)
+    emitter.emit('DRAWING', m)
+  })
+
+  const subscribe_cashOut = ws.subscribe({ action: ActionType.CASH_OUT }, (m: MessageMap[ActionType.CASH_OUT]) => {
+    console.log(`v [${ActionType[ActionType.CASH_OUT]}] ===> `, m)
+    emitter.emit('CASH_OUT', m)
   })
 
   const wsConnect = async (url: string, token: string) => {
@@ -74,7 +114,14 @@ export default function useWebsocket() {
     subscribe_drawing?.unsubscribe()
     subscribe_joinGAME?.unsubscribe()
     subscribe_openBet?.unsubscribe()
+    subscribe_closeBet?.unsubscribe()
+    subscribe_gameResult?.unsubscribe()
+    subscribe_betResult?.unsubscribe()
     subscribe_syncTimer?.unsubscribe()
+    subscribe_getHistory?.unsubscribe()
+    subscribe_updateOtherChips?.unsubscribe()
+    subscribe_getBallance?.unsubscribe()
+    subscribe_cashOut?.unsubscribe()
 
     ws.close()
   })
