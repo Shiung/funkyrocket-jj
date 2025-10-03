15563e0c1c3a50040dae3370f6ad8297c4811752
ptp_jackhsiung <ptp_jackhsiung@photons.com.tw>
2025-10-03 11:10:31 +0800

    [feat]: optimize mitt logic

--- src/core/mitt.ts
+++ src/core/mitt.ts
@@ -13,4 +13,27 @@ type EventFromEnum<E, M extends Record<number, any>> = {
     M[Extract<E[K], keyof M>]
 }
 
-export const emitter = mitt<Events & EventFromEnum<typeof ActionType, MessageMap>>()
+type MittEvents = Events & EventFromEnum<typeof ActionType, MessageMap>
+
+/** 
+ * [emitter] callback function
+ */
+export type HandlerOf<K extends keyof MittEvents> = (payload: MittEvents[K]) => void
+
+/**
+ * 
+ * @example
+ * ```ts
+ * emitter.on('unAuthorized', (message) => {}) 
+ * ```
+ * @param message 會推倒定義的型別
+ * 
+ * @example
+ * ```ts
+ * const callback: HandlerOf<'unAuthorized'> = (messageCb) => {}
+ * emitter.on('unAuthorized', callback)
+ * ```
+ * @param messageCb 會推倒定義的型別
+ * @description 透過 `type HandlerOf` 進行callback function 推導
+*/
+export const emitter = mitt<MittEvents>()
